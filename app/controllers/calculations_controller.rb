43d2041b3541190a1c2908ed1043441e15c48fb4
mdeschne <mdeschne@users.noreply.github.com>
2017-04-20 22:16:10 -0500

    loan and time

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -54,15 +54,13 @@ text = text.gsub(/[^a-z0-9\s]/i, "")
     # ================================================================================
 
   apr =  @apr
-  apr = apr/100.0
-  rate = ((1.0+(apr/12.0))**12.0)-1.0
-  rate = rate/12
-  months = @years*12.0
+  apr = apr/100
+  # rate = ((1+(apr/12))**12)-1
+  rate = apr/12
+  months = @years*12
   pal =  @principal
 
-  mid = 1 - (1 + rate)**-months
-  deno = mid/rate
-  pmt = pal / deno
+  pmt = (pal*rate)/(1-(1+rate)**(-months))
 
 
     @monthly_payment = pmt
@@ -87,12 +85,56 @@ text = text.gsub(/[^a-z0-9\s]/i, "")
     #   number of seconds as a result.
     # ================================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+starting = @starting.to_s
+
+split1 = starting.split
+
+date1 = split1[0]
+date1 = date1.to_s
+date1 = date1.gsub("-"," ")
+
+time1 = split1[1]
+time1 = time1.to_s
+time1 = time1.gsub(":"," ")
+time1 = time1.split
+seconds1 = time1[2]
+minutes1 = time1[1]
+hours1 = time1[0]
+seconds1 = seconds1.to_f
+minutes1 = minutes1.to_f
+hours1 = hours1.to_f
+
+ending = @ending.to_s
+
+split2 = ending.split
+
+date2 = split2[0]
+date2 = date2.to_s
+date2 = date2.gsub("-"," ")
+
+time2 = split2[1]
+time2 = time2.to_s
+time2 = time2.gsub(":"," ")
+time2 = time2.split
+seconds2 = time2[2]
+minutes2 = time2[1]
+hours2 = time2[0]
+seconds2 = seconds2.to_f
+minutes2 = minutes2.to_f
+hours2 = hours2.to_f
+
+
+sout = seconds2 - seconds1
+mout = minutes2 - minutes1
+hout = hours2 - hours1
+
+    @seconds = sout
+    @minutes = mout
+    @hours = hout
+    @days = time2.to_s
+    @weeks = split1.to_s
+    @years = @starting.to_s
+
 
     # ================================================================================
     # Your code goes above.
